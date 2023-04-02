dfc1f0b5e54afa917950bf1e2a651d0aa86649df
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-04-02 18:28:53 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Countdown Timer with embed spotify playlist
---
+++ src/components/First.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+export default class First extends Component {
+  render() {
+    return (
+      <h1>Exemple component</h1>
+    );
+  }
+}
