95d388160060846db44fd94d459273e2f0c87da9
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-04-07 17:11:14 -0300

    creates initial progress circle for timer

--- src/components/CircularProgressWithLabel.jsx
+++ src/components/CircularProgressWithLabel.jsx
@@ -1,9 +1,63 @@
 import { Component } from 'react';
+import { CircularProgress, Box, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
 
-export default class CircularProgressWithLabel extends Component {
+class CircularProgressWithLabel extends Component {
   render() {
+    const { value } = this.props;
+
     return (
-      <h1>Progress</h1>
+      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+      <CircularProgress variant="determinate" value={value} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography variant="caption" component="div" color="text.secondary">
+          {`${Math.round(value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+    );
+  }
+}
+
+CircularProgressWithLabel.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+export default class CircularStatic extends Component {
+  state = {
+    progress: 0,
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.updateProgress(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  updateProgress = () => {
+    this.setState((prevState, _) => ({
+      progress: prevState.progress >= 100 ? 0 : prevState.progress + 1
+    }))
+  }
+
+  render() {
+    const {progress} = this.state;
+
+    return (
+      <CircularProgressWithLabel value={progress}/>
     );
   }
 }
